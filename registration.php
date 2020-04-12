0871e9493dfb2c79a8396818c5f0c8253468e827
DarkhanAmanzhol <darhan25amanzhol@gmail.com>
2020-04-12 20:48:54 +0600

    register part only html

---
+++ registration.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+	<title>Login</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="logStyle.css" rel="stylesheet" type="text/css">
+</head>
+<body style="overflow-x: hidden;">
+	<header>
+		<a href="#">Go Home</a>
+	</header>
+<div class="container">
+	<h2>Register Now / <a href="login.php">Login Now</a></h2>
+	<div class="toPlace">
+		<form method="POST">
+			<input type="text" name="name" placeholder="Enter Your Name" class="writer"><br>
+			<input type="email" name="email" placeholder="Enter Your Email" class="writer"><br>
+			<input type="password" name="pass" placeholder="*******" class="writer"><br>
+			<input type="submit" name="submit" value="Submit" class="btn">
+			<input type="reset" name="reset" value="Reset" class="btn">
+		</form>
+	</div>
+</div>
+    <footer><p class="bot">&copy; Designed by Darkhan</p></footer>
+</body>
+</html>
+
+<?php
+
+?>
