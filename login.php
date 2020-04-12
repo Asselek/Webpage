4a8c294f84743d2cbb8ca97d6167e47e61da03e3
DarkhanAmanzhol <darhan25amanzhol@gmail.com>
2020-04-12 20:47:16 +0600

    log part only html

---
+++ login.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+	<title>Login</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="logStyle.css" rel="stylesheet" type="text/css">
+</head>
+<body style="overflow-x: hidden;">
+	<header>
+		<a href="#">Go Home</a>
+	</header>
+<div class="container">
+	<h2>Login Now / <a href="registration.php">Register Now</a></h2>
+	<div class="toPlace">
+		<form method="POST">
+			<input type="email" name="email" placeholder="Enter Your Email" class="writer"><br>
+			<input type="password" name="pass" placeholder="*******" class="writer"><br>
+			<input type="submit" name="login" value="Login" class="btn">
+			<input type="reset" name="reset" value="Reset" class="btn">
+		</form>
+	</div>
+</div>
+    <footer><p class="bot">&copy; Designed by Darkhan</p></footer>
+</body>
+</html>
+
+<?php
+
+?>
