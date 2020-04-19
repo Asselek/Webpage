acd73e097f6b87ca424c754d95d15fe63f478361
DarkhanAmanzhol <darhan25amanzhol@gmail.com>
2020-04-19 20:19:39 +0600

    Update login.php

--- login.php
+++ login.php
@@ -24,5 +24,51 @@
 </html>
 
 <?php
+session_start();
+$con = mysqli_connect("localhost","root","","user");
 
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	if(isset($_POST['login'])){
+		$v_email = mysqli_escape_string($con, $_POST['email']);
+		$v_pass = mysqli_escape_string($con, $_POST['pass']);
+
+		function validation($form_data){
+			$form_data = trim( stripcslashes( htmlspecialchars( $form_data ) ) );
+			return $form_data;
+		}
+		$email = validation($v_email);
+		$pass = validation($v_pass);
+
+		if(!empty($email) && !empty($pass)){
+			$email_check = "SELECT * FROM 'user_detail' WHERE 'u_email'='$email'";
+			$email_check_query = mysqli_query($con, $email_check);
+
+			if(mysqli_num_rows($email_check_query) == 1){
+				$fetch = mysqli_fetch_assoc($email_check_query);
+				$h_pass = $fetch['u_pass'];
+
+				if(password_verify($pass, $h_pass)){
+					$_SESSION['name'] = $fetch['u_name'];
+					$_SESSION['email'] = $fetch['u_email'];
+					$_SESSION['pass'] = $fetch['u_pass'];
+
+					if(isset($_POST['check'])){
+						setcookie('email',$email,time()+(86400 * 30));
+						setcookie('pass',$pass,time()+(86400 * 30));
+					}
+				}
+				else{
+					$msg = "Check your password or email";
+				}
+			}
+			else{
+				$msg = "Check your password or email";
+			}
+		}
+		else{
+			$msg = "Empty field";
+		}
+	}
+}
 ?>
