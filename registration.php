5141dbf1f8aa280008c5e282aa043cb5148d2e3f
DarkhanAmanzhol <darhan25amanzhol@gmail.com>
2020-04-25 14:35:11 +0600

    upgraded link of css

--- registration.php
+++ registration.php
@@ -2,7 +2,7 @@
 <head>
 	<title>Login</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="logStyle.css" rel="stylesheet" type="text/css">
+	<link href="logRegStyle.css" rel="stylesheet" type="text/css">
 </head>
 <body style="overflow-x: hidden;">
 	<header>
