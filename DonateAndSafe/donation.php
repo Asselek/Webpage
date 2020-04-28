84d18a41a5e8c036405881bda38ae0024474d243
Alikhan_T <52784852+Kai-Kairo@users.noreply.github.com>
2020-04-29 02:25:01 +0600

    Update donation.php

--- DonateAndSafe/donation.php
+++ DonateAndSafe/donation.php
@@ -1,4 +1,4 @@
-<<html>
+<html>
 <head>
   <title>Login</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -6,7 +6,7 @@
 </head>
 <body style="overflow-x: hidden; overflow-y: hidden;">
   <div class="header">
-  <a href="#" class="logo">Go Home</a>
+  <a href="/index.php" class="logo">Go Home</a>
   <div class="header-right">
   </div>
 </div>
@@ -38,26 +38,27 @@
           exit(0);
         }
 
-        $stid1 = oci_parse($conn, "SELECT COUNT(*) FROM DONATE");
-        $stid2 = oci_parse($conn, "SELECT AVG(AMOUNT) FROM DONATE");
-        $stid3 = oci_parse($conn, "SELECT SUM(AMOUNT) FROM DONATE");
-        $stid4 = oci_parse($conn, "SELECT MAX(AMOUNT) FROM DONATE");
+          $stid1 = oci_parse($conn, "SELECT COUNT(*) FROM DONATE");
+          $stid2 = oci_parse($conn, "SELECT TRUNC(AVG(AMOUNT), 3) FROM DONATE");
+          $stid3 = oci_parse($conn, "SELECT SUM(AMOUNT) FROM DONATE");
+          $stid4 = oci_parse($conn, "SELECT MAX(AMOUNT) FROM DONATE");
 
-        $e1 = oci_execute($stid1);
-        $e2 = oci_execute($stid2);
-        $e3 = oci_execute($stid3);
-        $e4 = oci_execute($stid4);
+          $e1 = oci_execute($stid1);
+          $e2 = oci_execute($stid2);
+          $e3 = oci_execute($stid3);
+          $e4 = oci_execute($stid4);
 
 
-        $row1 = oci_fetch_array($stid1, OCI_ASSOC+OCI_RETURN_NULLS);
-        $row2 = oci_fetch_array($stid2, OCI_ASSOC+OCI_RETURN_NULLS);
-        $row3 = oci_fetch_array($stid3, OCI_ASSOC+OCI_RETURN_NULLS);
-        $row4 = oci_fetch_array($stid4, OCI_ASSOC+OCI_RETURN_NULLS);     
+          $row1 = oci_fetch_array($stid1, OCI_ASSOC+OCI_RETURN_NULLS);
+          $row2 = oci_fetch_array($stid2, OCI_ASSOC+OCI_RETURN_NULLS);
+          $row3 = oci_fetch_array($stid3, OCI_ASSOC+OCI_RETURN_NULLS);
+          $row4 = oci_fetch_array($stid4, OCI_ASSOC+OCI_RETURN_NULLS);     
 
-        echo "<p>How many times have been donated: </p><p>".$row1['COUNT(*)']."</p>";
-        echo "<p>How much money do we get from donation: </p><p>".$row2['AVG(AMOUNT)']."</p>";
-        echo "<p>Average donation per person: </p><p>".$row3['SUM(AMOUNT)']."</p>";
-        echo "<p>Big donation: </p><p>".$row4['MAX(AMOUNT)']."</p>";
+          echo "<p>How many times have been donated: </p><p>".$row1['COUNT(*)']."</p>";
+          echo "<p>How much money do we get from donation: </p><p>".$row2['TRUNC(AVG(AMOUNT),3)']."</p>";
+          echo "<p>Average donation per person: </p><p>".$row3['SUM(AMOUNT)']."</p>";
+          echo "<p>Big donation: </p><p>".$row4['MAX(AMOUNT)']."</p>";          
+
     ?>
   </div>
 </div>
@@ -78,7 +79,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
   }
   
   if(!empty($nickname) && !empty($cardNumber) && !empty($amount)){
-    echo "<p class='error2'>Thanks for your donation!</p>";
+    echo nl2br("<p class='error2'>Thanks for your donation! \n Statistics will be updated soon</p>");
   }
   
   $conn = oci_connect('SYSTEM', '123', 'localhost/orcl');
@@ -87,20 +88,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     echo "<p class='error'>Oops somethings happened</p>";
     exit(0);
   }
-  echo "something";
-  $stid = oci_parse($conn, "INSERT INTO DONATE (DONATEID, AMOUNT, CARDNUMBER) VALUES (DONATION_ID_SEQ.nextval" . ",'" . $amount . "','" . $cardNumber . "')");
+  $stid = oci_parse($conn, "INSERT INTO DONATE (DONATEID, AMOUNT, CARDNUMBER, NICKNAME) VALUES (DONATION_ID_SEQ.nextval, $amount, '$cardNumber', '$nickname')");
   $exe = oci_execute($stid);
 
-  if(!$exe){
-    $e = oci_error($exe);
-    print htmlentities($e['message']);
-      print "\n<pre>\n";
-      print htmlentities($e['sqltext']);
-      printf("\n%".($e['offset']+1)."s", "^");
-      print  "\n</pre>\n";
-      die(1);
-  }
-  
 
   oci_close($conn);
 
